7ddd11c233b346909e3ad12f7143888da6704417
mttarvina <mttarvina@gmail.com>
2020-12-26 11:30:00 +0800

    Update1
    + Added Score, HighScore elements in top UI
    + Remaining bomb indicator turns RED when value is 0
    + Limited play area to 1000x650
    + Added exlode() method for bomb object
    + Explode timer and animation is now functional

--- daga_revA.py
+++ daga_revA.py
@@ -1,7 +1,6 @@
 import sys, pygame
 import math
 
-
 pygame.init()
 
 
@@ -11,67 +10,104 @@ bgColor = (255, 255, 255)                                                       
 screenSize = (1000, 800)                                                        # fix this to simplify distance scaling and all other calculations
 screenCenter = [(0.5*screenSize[0]), (0.5*screenSize[1])]                       # locate center of the screen
 
+
+# game globals
 gameClock = pygame.time.Clock()
+gameIsOnStart = False                                                           # set this to false for now
+gameIsActive = True                                                             # set this to True for now
+gameIsOver = False                                                              # set this to False for now
+score = 0
+hiScore = 0
+maxBombs = 5
+bombs = []
+bombIndex = 0
+bombDropped = False
+bombExploded = False
+bombExpired = False
+bombToRemove = None
+bombToExplode = None
+
 
 # create screen
 screen = pygame.display.set_mode(screenSize)
+
 
 # title and icon
 pygame.display.set_caption('Daga: Remote 2D Space Mapping Project')
 icon = pygame.image.load('logo.ico')
 pygame.display.set_icon(icon)
 
-# fps display
-fpsFont = pygame.font.Font('freesansbold.ttf', 20)
-displayFps = True                                                               # set this to True for now
-def renderFps(fpsVal):
+
+# top UI
+fpsFont = pygame.font.Font('freesansbold.ttf', 16)
+bombFont = pygame.font.Font('freesansbold.ttf', 24)
+scoreFont = pygame.font.Font('freesansbold.ttf', 36)
+hiScoreFont = pygame.font.Font('freesansbold.ttf', 24)
+compassImg = pygame.image.load('./assets/compass.png').convert_alpha()
+compassPos = compassImg.get_rect(center=(75, 75))
+uiMargin = [(5,5), (5,145), (995, 145), (995, 5)]
+
+def renderTopUI(fpsVal, bombList, bombLimit):
+    # render margin
+    pygame.draw.polygon(screen, (0,0,0), uiMargin, width=3)
+
+    # render compass
+    screen.blit(compassImg, compassPos)                                         # draw compass
+
+    # render fps
     fpsText = fpsFont.render("FPS: {}".format(int(fpsVal)), True, (0, 0, 0))
     screen.blit(fpsText, (900, 25))
 
-# bomb count display
-bombFont = pygame.font.Font('freesansbold.ttf', 32)
-def renderActiveBombs(bombList, bombLimit):
-    counter = 0
-    for bomb in bombList:
-        if bomb.active:
-            counter += 1
-    bombText = bombFont.render("Bombs Remaining: {}".format(bombLimit - counter), True, (0, 0, 0))
-    screen.blit(bombText, (330, 50))
+    # render remaining bombs
+    if gameIsActive:
+        counter = 0
+        for bomb in bombList:
+            if bomb.active:
+                counter += 1
+        if counter < bombLimit:
+            bombText = bombFont.render("Bombs Remaining: {}".format(bombLimit - counter), True, (0, 0, 0))
+        else:
+            bombText = bombFont.render("Bombs Remaining: {}".format(bombLimit - counter), True, (255, 0, 0))
+        screen.blit(bombText, (375, 100))
 
-# compass image
-compassImg = pygame.image.load('./assets/compass.png').convert_alpha()
-compassPos = compassImg.get_rect(center=(64, 64))
+    # render Score
+    scoreText = scoreFont.render("SCORE: {}".format(score), True, (0, 0, 0))
+    screen.blit(scoreText, (410, 50))
+
+    # render High Score
+    hiScoreText = hiScoreFont.render("HIGH SCORE: {}".format(hiScore), True, (0, 0, 0))
+    screen.blit(hiScoreText, (750, 100))
 
 
 # ******************************************************************************
 # --- define a mobot object to display mobot location, movement, and oriention
 class dagaObject:
-    def __init__(self, inputImgPath, inputLoc, inputMoveSpeed, inputRotateSpeed):
-        self.imageRef = pygame.image.load(inputImgPath).convert_alpha()
+    def __init__(self, _loc, _mSpeed, _rSpeed):
+        self.imageRef = pygame.image.load('./assets/dagaPlane.png').convert_alpha()
         self.image = self.imageRef.copy()
-        self.loc = inputLoc
-        self.mSpeed = inputMoveSpeed
-        self.rSpeed = inputRotateSpeed
+        self.loc = _loc
+        self.mSpeed = _mSpeed
+        self.rSpeed = _rSpeed
         self.pos = self.image.get_rect(center=(self.loc[0], self.loc[1]))
         self.angle = 0
-        self.vDir = None
-        self.rDir = None
+        self.hDir = None                                                        # True if FORWARD, False if BACKWARD
+        self.rDir = None                                                        # True if CCW, False if CW
         self.turnFlag = False
         self.moveFlag = False
 
     def turn(self):
-        if self.rDir == 'ccw':
+        if self.rDir == True:
             self.angle += self.rSpeed
-        elif self.rDir == 'cw':
+        elif self.rDir == False:
             self.angle -= self.rSpeed
         self.angle = self.angle % 360 
         self.image = pygame.transform.rotate(self.imageRef, self.angle)         # rotate the original image -- to minimize image distortion
         self.pos = self.image.get_rect(center=(self.loc[0], self.loc[1]))       # fix the center location
 
     def move(self):
-        if self.vDir == 'forward':
+        if self.hDir == True:
             angleTemp = self.angle
-        elif self.vDir == 'backward':
+        elif self.hDir == False:
             if self.angle < 180:
                 angleTemp = 180 + self.angle
             else:
@@ -82,14 +118,14 @@ class dagaObject:
         self.loc[1] = self.loc[1] - deltaY
 
         # check if mobot is already at the edge of the screen, keep it inside the screen
-        if self.loc[0] <= 0: 
-            self.loc[0] = 0
-        if self.loc[0] >= screenSize[0]:
-            self.loc[0] = screenSize[0]
-        if self.loc[1] <= 0: 
-            self.loc[1] = 0
-        if self.loc[1] >= screenSize[1]:
-            self.loc[1] = screenSize[1]
+        if self.loc[0] <= dagaSize*0.5: 
+            self.loc[0] = dagaSize*0.5
+        if self.loc[0] >= screenSize[0] - (dagaSize*0.5):
+            self.loc[0] = screenSize[0] - (dagaSize*0.5)
+        if self.loc[1] <= 150 + dagaSize*0.5: 
+            self.loc[1] = 150 + dagaSize*0.5
+        if self.loc[1] >= screenSize[1] - (dagaSize*0.5):
+            self.loc[1] = screenSize[1] - (dagaSize*0.5)
             
         self.pos = self.image.get_rect(center=(self.loc[0], self.loc[1]))
 # *** end of dataObject definition
@@ -100,102 +136,115 @@ class bombObject:
     def __init__(self, _index):
         self.imgDropped = pygame.image.load('./assets/bomb_ready.png').convert_alpha()
         self.imgExplode = pygame.image.load('./assets/bomb_exploded.png').convert_alpha()
+        self.imgage = None
         self.pos = None
-        self.timer = 3000
-        self.timerEvent = pygame.USEREVENT + _index
-        pygame.time.set_timer(self.timerEvent , self.timer)
+        self.timerDrop = 3000
+        self.timerDropEvent = pygame.USEREVENT + _index
+        self.timerFire = 1000
+        self.timerFireEvent = pygame.USEREVENT + 10 + _index                    # put a +10 offset so USEREVENTs are unique
         self.active = False
 
     def drop(self, _droppedLoc):
-        self.pos = self.imgDropped.get_rect(center=(_droppedLoc[0], _droppedLoc[1]))
-        screen.blit(self.imgDropped, self.pos)
         self.active = True
+        self.fire = False
+        self.image = self.imgDropped.copy()                                     # use dropped version of the bomb
+        pygame.time.set_timer(self.timerDropEvent , self.timerDrop)             # start the dropped timer here
+        self.pos = self.imgDropped.get_rect(center=(_droppedLoc[0], _droppedLoc[1]))
+
+    def explode(self):
+        if self.active:
+            self.active = True
+            self.image = self.imgExplode.copy()                                 # use explode version of the bomb
+            pygame.time.set_timer(self.timerFireEvent , self.timerFire)         # start the explode timer here
+        else:
+            pass
 
     def disappear(self):
         self.active = False
-        self.pos = self.pos.move(5000, 5000)
+        pygame.time.set_timer(self.timerFireEvent , 0)                          # disable drop event timer
+        pygame.time.set_timer(self.timerFireEvent , 0)                          # disable explode event timer
+        #self.pos = self.pos.move(5000, 5000)
 # *** end of bombObject definition
 
 
-mobot = dagaObject('./assets/dagaPlane.png', screenCenter, 3, 3)
-bombIndex = 0
-bombDropped = False
-bombExpired = False
-maxBombs = 5
-bombs = []
-bombToRemove = None
+# create player object
+mobot = dagaObject(screenCenter, 3, 3)
 
 
 # ******************************************************************************
 # --- GAME LOOP
 while True:
-    for wEvent in pygame.event.get():
-        if wEvent.type == pygame.QUIT:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
             sys.exit()
-        if wEvent.type == pygame.KEYDOWN:
-            if wEvent.key == pygame.K_LEFT:
-                mobot.rDir = 'ccw'
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                mobot.rDir = True
                 mobot.turnFlag = True
-            if wEvent.key == pygame.K_RIGHT:
-                mobot.rDir = 'cw'
+            if event.key == pygame.K_RIGHT:
+                mobot.rDir = False
                 mobot.turnFlag = True
-            if wEvent.key == pygame.K_UP:
-                mobot.vDir = 'forward'
+            if event.key == pygame.K_UP:
+                mobot.hDir = True
                 mobot.moveFlag = True
-            if wEvent.key == pygame.K_DOWN:
-                mobot.vDir = 'backward'
+            if event.key == pygame.K_DOWN:
+                mobot.hDir = False
                 mobot.moveFlag = True
-        if wEvent.type == pygame.KEYUP:
-            if wEvent.key == pygame.K_LEFT or wEvent.key == pygame.K_RIGHT:
+            if event.key == pygame.K_SPACE and not bombDropped:
+                bombDropped = True
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                 mobot.rDir = None
                 mobot.turnFlag = False
-            if wEvent.key == pygame.K_UP or wEvent.key == pygame.K_DOWN:
-                mobot.vDir = None
+            if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+                mobot.hDir = None
                 mobot.moveFlag = False
-            if wEvent.key == pygame.K_SPACE and not bombDropped:
-                bombDropped = True
+            if event.key == pygame.K_SPACE:
+                bombDropped = False
         for i in range(maxBombs):
-            if wEvent.type == pygame.USEREVENT + i:
+            if event.type == pygame.USEREVENT + i:
+                bombExploded = True
+                bombToExplode = i
+        for i in range(maxBombs):
+            if event.type == pygame.USEREVENT + 10 + i:
                 bombExpired = True
                 bombToRemove = i
 
+    screen.fill(bgColor)                                                        # draw background    
 
-    screen.fill(bgColor)                                                        # draw background
-    if displayFps:                                                              # display fps
-        renderFps(gameClock.get_fps())
-    screen.blit(compassImg, compassPos)                                         # draw compass
+    if gameIsActive:
+        if mobot.turnFlag:
+            mobot.turn()
+        if mobot.moveFlag:
+            mobot.move()
+        screen.blit(mobot.image, mobot.pos)                                     # draw mobot
 
-    if mobot.turnFlag:
-        mobot.turn()
-    if mobot.moveFlag:
-        mobot.move()
-    screen.blit(mobot.image, mobot.pos)                                         # draw mobot
-
-    if bombExpired:                                                             # remove any bomb that expired
-        bombs[bombToRemove].disappear()
-        bombExpired = False
-    if bombDropped:                                                             # draw any new bomb that was dropped
-        if bombIndex >= maxBombs:
-            bombIndex = 0
-        if len(bombs) >= maxBombs:
-            if bombs[bombIndex].active:
-                pass
+        if bombExploded:                                                        # changed bomb image to exploded version, start explode timer
+            bombs[bombToExplode].explode()
+            bombExploded = False
+        if bombExpired:                                                         # remove any bomb that expired
+            bombs[bombToRemove].disappear()
+            bombExpired = False
+        if bombDropped:                                                         # draw any new bomb that was dropped
+            if bombIndex >= maxBombs:
+                bombIndex = 0
+            if len(bombs) >= maxBombs:
+                if bombs[bombIndex].active:                                     # do not draw bomb if current bomb index is still active
+                    pass
+                else:
+                    bombs[bombIndex].__init__(bombIndex)                        # reinitialize bomb
+                    bombs[bombIndex].drop(mobot.loc)                            # reactivate bomb status, start drop timer, update location
+                    bombIndex += 1                                              
             else:
-                bombs[bombIndex].__init__(bombIndex)
-                bombs[bombIndex].drop(mobot.loc)
+                bombs.append(bombObject(bombIndex))                             # if bomb list is not full
+                bombs[bombIndex].drop(mobot.loc)                                # activate the bomb, start timer, define location
                 bombIndex += 1
-        else:
-            bombs.append(bombObject(bombIndex))
-            bombs[bombIndex].drop(mobot.loc)
-            bombIndex += 1
-        bombDropped = False
-    if bombs:
-        for bomb in bombs:
+            bombDropped = False
+        for bomb in bombs:                                                      # draw only active bombs
             if bomb.active:
-                screen.blit(bomb.imgDropped, bomb.pos)
-    
-    renderActiveBombs(bombs, maxBombs)
+                screen.blit(bomb.image, bomb.pos)
 
+    renderTopUI(gameClock.get_fps(), bombs, maxBombs)                           # draw all elements of the top UI
     pygame.display.update()
     gameClock.tick(120)                                                         # limit fps to 120
 # *** end of GAME LOOP
